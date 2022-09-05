f2b8417083edeba4a6e04b080dcb2e14d0338cc1
Javierklie <javierklie1@gmail.com>
2022-09-05 02:54:45 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let quien = ["perro", "hermano", "sobrino", "pajaro"];
+  let accion = ["se comió", "molesta", "juega", "corrio"];
+  let que = [
+    "antes de la clase",
+    "justo a tiempo",
+    "todo el tiempo",
+    "durante mi almuerzo",
+    "durante mi entrenamiento"
+  ];
+
+  let excuseP = document.querySelector("#excuse");
+  excuseP.textContent =
+    "Mi" +
+    " " +
+    quien[Math.floor(Math.random() * 4)] +
+    " " +
+    accion[Math.floor(Math.random() * 4)] +
+    " " +
+    que[Math.floor(Math.random() * 4)];
+
   console.log("Hello Rigo from the console!");
 };
